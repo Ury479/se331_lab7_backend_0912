19a52e647e7de0c8db0118e6e8751c0103dd54c6
Ur7479 <yihao_w@cmu.ac.th>
2025-09-02 11:42:26 +0700

    Q2

---
+++ src/main/java/com/example/demo331bacnkend/Demo331BacnkendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo331bacnkend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo331BacnkendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo331BacnkendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo331bacnkend/Demo331BacnkendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo331bacnkend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo331BacnkendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
